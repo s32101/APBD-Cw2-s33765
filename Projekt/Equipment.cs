6ed838c978a9b6dd6246e5dd2c979ed59aeb107c
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-19 13:29:13 +0100

    Equipment added

---
+++ Projekt/Equipment.cs
@@ -0,0 +1,14 @@
+﻿namespace Projekt;
+
+public class Equipment
+{
+    public Guid Id { get; }
+    public bool Availibility { get; set; }
+
+    Equipment()
+    {
+        Id = Guid.NewGuid();
+        Availibility = true;
+        
+    }
+}
--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace  Projekt;
+
+
+public class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Start of the program.");
+    }
+}
