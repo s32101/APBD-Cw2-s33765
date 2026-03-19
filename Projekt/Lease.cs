61df742201c3e8f3a38f3627a543a45885873c35
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-19 13:46:57 +0100

    Started working on Lease.cs

---
+++ Projekt/Lease.cs
@@ -0,0 +1,39 @@
+﻿namespace Projekt;
+
+public class Lease
+{
+    private int TimeToReturn = 14; // W dniach
+    public Equipment equipment { get; }
+    DateTime LeaseDate;
+    DateTime ExpiryDate;
+    
+    DateTime ReturnDate;
+    
+    Lease(Equipment equipment)
+    {
+        this.equipment = equipment;
+        this.equipment.Availibility = false;
+        LeaseDate = DateTime.Now;
+        ExpiryDate = LeaseDate.AddDays(TimeToReturn);
+    }
+
+
+    public void Return()
+    {
+        ReturnDate = DateTime.Now;
+        this.equipment.Availibility = true;
+        if (ReturnDate > ExpiryDate)
+        {
+            calculateCosts();
+        }
+        
+    }
+
+    int calculateCosts()// opłata = 20 złotych za każdy dzień opóźnienia
+    {
+        TimeSpan timeSpan = ExpiryDate - ReturnDate;
+        int costs = 20 * timeSpan.Days;
+        return costs;
+    }
+    
+}
